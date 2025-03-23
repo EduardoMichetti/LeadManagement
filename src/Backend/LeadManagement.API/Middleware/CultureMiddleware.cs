814c9bbb1dd821a03f7b80ecb248f79eb1c50f51
eduardo.michetti <eduardo.michetti@dtidigital.com.br>
2025-03-23 20:25:34 -0300

    feat: Criando middleware para interceptar linguagem. Chamar arquivo de constantes conforme linguagem

---
+++ src/Backend/LeadManagement.API/Middleware/CultureMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace LeadManagement.API.Middleware;
+
+public class CultureMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public CultureMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    public async Task Invoke(HttpContext context)
+    {
+        var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures);
+
+        var requestedculture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+        var cultureInfo = new CultureInfo("en");
+
+        if (!string.IsNullOrWhiteSpace(requestedculture)
+            && supportedLanguages.Any(c => c.Name.Equals(requestedculture)))
+        {
+            cultureInfo = new CultureInfo(requestedculture);
+        }
+
+        CultureInfo.CurrentCulture = cultureInfo;
+        CultureInfo.CurrentUICulture = cultureInfo;
+
+        await _next(context);
+    }
+}
