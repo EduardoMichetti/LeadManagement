a77da5d7d44f4405eaa591480be5db308ac389db
eduardo.michetti <eduardo.michetti@dtidigital.com.br>
2025-03-23 19:27:39 -0300

    feat: Criando classe de constantes para mensagens de exceções

--- src/Backend/LeadManagement.Application/UseCases/Lead/Register/RegisterLeadValidator.cs
+++ src/Backend/LeadManagement.Application/UseCases/Lead/Register/RegisterLeadValidator.cs
@@ -5,13 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using FluentValidation;
 using LeadManagement.Communication.Requests;
+using LeadManagement.Exceptions;
 
 namespace LeadManagement.Application.UseCases.Lead.Register;
 public class RegisterLeadValidator : AbstractValidator<RequestRegisterLeadJson>
 {
     public RegisterLeadValidator()
     {
-        RuleFor(lead => lead.ContactFirstName).NotEmpty().WithMessage("ContactFirstName is required");
+        RuleFor(lead => lead.ContactFirstName).NotEmpty().WithMessage(ResourceMessagesException.CONTACT_FIRST_NAME);
         RuleFor(lead => lead.ContactEmail).NotEmpty().WithMessage("ContactEmail is required");
         RuleFor(lead => lead.ContactEmail).EmailAddress();
     }
